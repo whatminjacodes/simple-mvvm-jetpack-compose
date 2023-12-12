6a6b90d9457961b7af9aeb5f657053f4dcd6b448
project <whatminjacodes@gmail.com>
2023-12-12 12:33:05 +0200

    First screen navigation

--- simple-mvvm-jetpack-compose/app/src/main/java/com/example/simplemvvmexamplewithjetpackcompose/NavGraph.kt
+++ simple-mvvm-jetpack-compose/app/src/main/java/com/example/simplemvvmexamplewithjetpackcompose/NavGraph.kt
@@ -5,12 +5,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.simplemvvmexamplewithjetpackcompose.Destinations.DASHBOARD_ROUTE
 import com.example.simplemvvmexamplewithjetpackcompose.Destinations.WELCOME_ROUTE
+import com.example.simplemvvmexamplewithjetpackcompose.main.DashBoardRoute
 import com.example.simplemvvmexamplewithjetpackcompose.start.WelcomeRoute
 
 object Destinations {
-    const val SIGN_IN = "signin"
-    const val SIGN_UP = "signup"
     const val WELCOME_ROUTE = "welcome"
     const val DASHBOARD_ROUTE = "dashboard/{email}"
 }
@@ -19,10 +19,15 @@ fun NavGraph(navController: NavHostController = rememberNavController()) {
     NavHost(navController = navController, startDestination = WELCOME_ROUTE) {
         composable(WELCOME_ROUTE) {
             WelcomeRoute(OnSignInClicked = {
-                //navController.navigate(SIGN_IN)
+                navController.navigate(DASHBOARD_ROUTE)
             }, OnSignUpClicked = {
-                //navController.navigate(SIGN_UP)
+                navController.navigate(DASHBOARD_ROUTE)
             })
+        }
+
+        composable(DASHBOARD_ROUTE){
+            val message  = "message from start screen"
+            DashBoardRoute(messageFromStart = message!!)
         }
     }
 }
---
+++ simple-mvvm-jetpack-compose/app/src/main/java/com/example/simplemvvmexamplewithjetpackcompose/main/DashBoardRoute.kt
@@ -0,0 +1,8 @@
+package com.example.simplemvvmexamplewithjetpackcompose.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashBoardRoute(messageFromStart: String) {
+    FirstScreen(messageFromStart = messageFromStart)
+}
